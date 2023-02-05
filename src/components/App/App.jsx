eebf827137df1691ae068a80c1cc581b4deda686
Yuliya Zozulya <djulia@ukr.net>
2023-02-06 00:26:35 +0200

    adds fonts

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,28 +1,35 @@
-import { Wrapper, Container, HeroTitle } from './App.styled';
+import {
+  HeroWrapper,
+  Container,
+  HeroTitle,
+  ProductsListButton,
+} from './App.styled';
 import { BiAlignLeft } from 'react-icons/bi';
 
 export const App = () => {
   return (
-    <Wrapper>
-      <Container>
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            marginBottom: '250px',
-          }}
-        >
-          <img
-            src={require('../../images/logo.png')}
-            alt="Logo"
-            width="216"
-            height="52"
-          ></img>
-          <BiAlignLeft size={50} color="white"></BiAlignLeft>
-        </div>
-        <HeroTitle>Expensive and rich</HeroTitle>
-        <div>product catalog</div>
-      </Container>
-    </Wrapper>
+    <>
+      <HeroWrapper>
+        <Container>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              marginBottom: '250px',
+            }}
+          >
+            <img
+              src={require('../../images/logo.png')}
+              alt="Logo"
+              width="216"
+              height="52"
+            ></img>
+            <BiAlignLeft size={50} color="white"></BiAlignLeft>
+          </div>
+          <HeroTitle>Expensive and rich</HeroTitle>
+          <ProductsListButton>product catalog</ProductsListButton>
+        </Container>
+      </HeroWrapper>
+    </>
   );
 };
--- src/components/App/App.styled.js
+++ src/components/App/App.styled.js
@@ -1,13 +1,14 @@
 import styled from '@emotion/styled';
 import BCG from '../../images/MainBCG.jpg';
 
-export const Wrapper = styled.div`
+export const HeroWrapper = styled.div`
   margin: 0 auto;
   text-align: center;
   background-image: url(${BCG});
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
+  box-shadow: rgba(0, 0, 0, 0.25);
 `;
 export const Container = styled.div`
   padding: 20px;
@@ -16,8 +17,21 @@ export const Container = styled.div`
 export const HeroTitle = styled.h1`
   margin-bottom: 50px;
   color: #ffffff;
-  font-family: 'Noto Serif', 'Roboto', serif;
+  font-family: 'Duru sans', 'Roboto', serif;
   font-size: 36px;
   font-weight: bold;
   line-height: 1.5;
+`;
+
+export const ProductsListButton = styled.button`
+  display: inline-block;
+  width: 261px;
+  height: 51px;
+  background-color: #000000;
+  border: 1px solid #ffffff;
+  color: #ffffff;
+  text-transform: uppercase;
+  font-family: 'Duru sans', 'Roboto', serif;
+  font-size: 18px;
+  line-height: 1;
 `;
